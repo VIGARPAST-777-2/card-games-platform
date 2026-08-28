8c9b6af55ced66ba566250676d7aa461a2f0694b
VIGARPAST_777 <vicgarpas3@alu.edu.gva.es>
2026-08-28 19:35:47 +0200

    Routes for store friends clubs pass auth

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,23 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
 import { HomePage } from './pages/HomePage';
 import { PlayPage } from './pages/PlayPage';
 import { ProfilePage } from './pages/ProfilePage';
-import { Layout } from './components/Layout';
+import { AuthPage } from './pages/AuthPage';
+import { StorePage } from './pages/StorePage';
+import { FriendsPage } from './pages/FriendsPage';
+import { ClubsPage } from './pages/ClubsPage';
+import { PassPage } from './pages/PassPage';
+import { useAuthStore } from './store/authStore';
 
 export default function App() {
+  const init = useAuthStore((s) => s.init);
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return (
     <Layout>
       <Routes>
@@ -12,6 +25,11 @@ export default function App() {
         <Route path="/play" element={<PlayPage />} />
         <Route path="/play/:matchId" element={<PlayPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/store" element={<StorePage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+        <Route path="/clubs" element={<ClubsPage />} />
+        <Route path="/pass" element={<PassPage />} />
       </Routes>
     </Layout>
   );
